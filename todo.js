02be8231479518ccb569d2923622ccb035c4a6e9
perebaj <perebaj@gmail.com>
2019-03-02 09:16:54 -0300

    documentacao

--- todo.js
+++ todo.js
@@ -1,3 +1,9 @@
+/**
+ * @author Jonathan Silva
+ * @Data March 01, 2019
+ * @Brief : Esse aquivo contém as variaveis que eventualmente serão usaram para fazer o script em JavaScript e suas funçoes.
+ *  Essas funçoes, são reponsáveis pela lógica do jogo e do ScoreBoard
+ */
 const userScopre = 0;
 const computerScore = 0;
 const userScore_span = document.getElementById("user-score");
@@ -7,23 +13,33 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissor_div = document.getElementById("scissor");
-
+/**
+ * @Brief   Administa as escolhas aleatorias do computador
+ * return   Retorna uma das tres jogadas possiveis do computador
+ */
 function getComputerChoice(){
-    const choices = ['r','p','s'];
+    const choices = ['rock','paper','scissor'];
     const randomNumber = (Math.floor(Math.random() * 3));
     return choices[randomNumber];
 }
-
+/**
+ * @Brief   Resposável pela lógica do game
+ * @param {Escolha do usuário} userChoice 
+ */
 function game(userChoice){
     const computerChoice = getComputerChoice();
-    console.log(computerChoice);
+    console.log("Computer choice =>" + computerChoice);
+    console.log("User choice =>" + userChoice); 
+
 }
 
-
+/**
+ * @Brief   
+ */
 function main(){
 
     rock_div.addEventListener('click', function(){
-        game("rock");
+        game("rock"); //Interação de click do cursor do mouse com as userChoices
     })
     
     paper_div.addEventListener('click', function(){
